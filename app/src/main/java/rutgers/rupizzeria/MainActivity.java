a44678ea5b845e13de3985d1aa309c80246d0d5f
csaguil0926 <csaguil0926@gmail.com>
2022-12-05 21:19:07 -0500

    made bottom nav

--- app/src/main/java/rutgers/rupizzeria/MainActivity.java
+++ app/src/main/java/rutgers/rupizzeria/MainActivity.java
@@ -1,14 +1,46 @@
 package rutgers.rupizzeria;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import rutgers.rupizzeria.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new OrderPizzaFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener( item -> {
+            switch (item.getItemId()) {
+                case R.id.orderPizza:
+                    replaceFragment(new OrderPizzaFragment());
+                    break;
+                case R.id.storeOrders:
+                    replaceFragment(new StoreOrdersFragment());
+                    break;
+                case R.id.cart:
+                    replaceFragment(new CartFragment());
+                    break;
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
