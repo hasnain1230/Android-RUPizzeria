605dec5935adf14c00de7e1626205fb5a2e0574d
Hasnain <hasnain1230@gmail.com>
2022-12-06 16:49:43 -0500

    Upgraded to JUnit 5.9.0

---
+++ app/src/test/java/rutgers/rupizzeria/OrderPizzaFragmentTest.java
@@ -0,0 +1,13 @@
+package rutgers.rupizzeria;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OrderPizzaFragmentTest {
+
+    @Test
+    void onCreate() {
+        assertEquals(10,10);
+    }
+}
